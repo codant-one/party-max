e027a3d407506fdfa920a78ef9e846b9c757d6dd
SteffaniCastro <steffani.castro.useche@gmail.com>
2023-10-27 02:02:25 -0400

    fix home

--- src/components/app/Footer.vue
+++ src/components/app/Footer.vue
@@ -8,7 +8,7 @@
     <VContainer class="mt-10">
       
 
-        <VRow no-gutters  class="tw-text-terciary">
+        <VRow no-gutters  class="tw-text-tertiary">
           <VCol cols="12" md="3" class="d-flex align-center hr">
             <span>Conoce todas nuestras ofertas</span>
           </VCol> 
