e73e41a73ed0fadf0c27d9b4657473ac5925ad56
Diego Bolivar <Dbolivarv90@gmail.com>
2023-10-31 16:58:14 -0500

    Create initial of views products and categories

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -1,13 +1,85 @@
 <script setup lang="ts">
 
+import banner_principal from '@/assets/images/banner1_category.png';
+import banner2_category from '@/assets/images/banner2_category.png';
+import banner3_category from '@/assets/images/banner3_category.png';
+import banner4_category from '@/assets/images/banner4_category.png';
+
+
+
 
 </script>
 
 <template>
  
+  <!--Banner Principal-->
+  <VContainer class="content py-13 text-center w-100">
+   
+    <VCard class="mt-7 no-shadown card-information p-0">
+      <VCardItem class="p-0">
+        <VImg :src="banner_principal" cover/>
+      </VCardItem>  
+    </VCard>
 
-  <VContainer class="content py-13 text-center w-100" style="height: 400px">
-   CATEGORIES
+    <!-- novedades -->
+    <VCard class="mt-7 no-shadown card-information p-0">
+      <VCardTitle class="px-7 py-3 d-flex align-center">
+        <span>Novedades</span>
+        <VSpacer />
+        <router-link to="/suppliers" class="ms-5 tw-no-underline tw-text-tertiary font-size-16 me-3 hover:tw-text-primary">Ver todos</router-link>
+      </VCardTitle>
+      <VDivider />
+      <VCardText class="px-7 mt-5 mb-5 d-flex align-items-stretch justify-content-between">
+       
+      </VCardText>  
+    </VCard>
+
+    <!-- banner 2 , banner 3-->
+    <VCard class="mt-7 no-shadown card-information p-0 d-flex transparent">
+        <VCard class="no-shadown card-information p-0 w-50">
+          <router-link to="/categories" class="tw-no-underline">
+            <VCardItem class="p-0">
+              <VImg :src="banner2_category" cover/>
+            </VCardItem>  
+          </router-link>
+        </VCard>
+        <VCard class="no-shadown card-information p-0 w-50 ms-5">
+          <router-link to="/categories" class="tw-no-underline">
+            <VCardItem class="p-0">
+              <VImg :src="banner3_category" cover/>
+            </VCardItem>  
+          </router-link>
+        </VCard>
+    </VCard>
+
+      <!--Banner 4-->
+    <VCard class="mt-7 no-shadown card-information p-0">
+      <VCardItem class="p-0">
+        <VImg :src="banner4_category" cover/>
+      </VCardItem>  
+    </VCard>
+    <VCard class="mt-7 no-shadown card-information transparent p-0">  
+            <v-btn class="tw-bg-primary tw-text-white button-product" rounded="xl" block>Ver todos los productos</v-btn>
+    </VCard>
+
     
   </VContainer>
 </template>
+
+<style scoped>
+
+.transparent {
+    background: transparent !important;
+  }
+
+.button-product
+{
+  font-size: 16px!important;
+  font-weight: 700;
+  padding: 24px!important;
+}
+.button-product:hover{
+    background-color: #FF27B3 !important;
+    box-shadow: 0px 0px 24px 0px #FF27B3;
+}
+</style>
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,4 +1,24 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+const items = ref([
+  { title: 'Item 01',value: 1, },
+  { title: 'Item 02',value: 2, },
+  { title: 'Item 03',value: 3, },
+  { title: 'Item 04',value: 4, },
+  { title: 'Item 05',value: 5, },
+  { title: 'Item 06',value: 6, },
+])
+
+const items_check = ref([
+  { name: 'item 1 (2)', selected: false },
+  { name: 'item 2 (3)', selected: false },
+  { name: 'item 3 (4)', selected: false },
+])
+
+ const priceRange = [0, 1000] // Valores iniciales del rango de precios
+ const minPrice = 0 // Valor mínimo del rango
+ const maxPrice = 1000 // Valor máximo del rango
+
 
 
 </script>
@@ -6,8 +26,97 @@
 <template>
  
 
-  <VContainer class="content py-13 text-center w-100" style="height: 400px">
-    PRODUCTS
+  <VContainer class="content py-13 text-center w-100">
+    
+    <VRow no-gutters class="transparent">
+
+      <VCol cols="12" md="3">
+        <VCard class="mt-7 no-shadown card-information sidebar-container">
+            <VCardItem class="p-0 text-left mb-5">
+              CATEGORÍAS
+            </VCardItem> 
+            
+            <v-list :items="items" class="text-left"></v-list>
+
+        </VCard>
+
+        <VCard class="mt-7 no-shadown card-information sidebar-container">
+
+            <VCardItem class="p-0 text-left mb-5">
+              PROVEEDORES
+            </VCardItem>
+
+            <v-card-text class="text-left align-left p-0">
+                <v-text-field
+                density="compact"
+                variant="solo"
+                label=""
+                append-inner-icon="mdi-magnify"
+                single-line
+                hide-details
+              ></v-text-field>
+            </v-card-text>
+
+            <v-checkbox
+              v-for="(item, index) in items_check"
+              :key="index"
+              v-model="item.selected"
+              :label="item.name"
+            ></v-checkbox>
+
+            <v-row class="align-center">
+              <v-col cols="12">
+                <v-slider
+                  v-model="priceRange"
+                  :min="minPrice"
+                  :max="maxPrice"
+                  thumb-label="always"
+                  thumb-color="primary"
+                  track-color="primary"
+                ></v-slider>
+              </v-col>
+            </v-row>
+            <v-row class="align-center">
+              <v-col cols="6">
+                <v-text-field    
+                  v-model="minPrice"
+                  label="Precio Mínimo"
+                  outlined
+                  
+                ></v-text-field>
+              </v-col>
+              <v-col cols="6">
+                <v-text-field
+                  v-model="maxPrice"
+                  label="Precio Máximo"
+                  outlined
+                ></v-text-field>
+              </v-col>
+            </v-row>
+
+
+        </VCard>  
+      </VCol> 
+      
+      <VCol cols="12" md="9">
+        mosaico de productos
+      </VCol>
+
+    </VRow>  
+
     
   </VContainer>
 </template>
+
+
+
+<style scoped>
+.sidebar-container
+{
+  padding: 25px 20px;
+}
+
+
+
+
+</style>
