2f611da3d585b7b20766da6eeadf88ecc1643c94
Diego Bolivar <Dbolivarv90@gmail.com>
2023-11-09 19:36:16 -0500

    ajustes view products

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,8 +1,11 @@
-<script setup lang="ts">
+<script setup>
 import { ref } from 'vue'
-
+import { useHomeStores } from '@/stores/home'
 import icon1 from '@/assets/icons/icon-menu-product.svg'
 import icon2 from '@/assets/icons/icon-menu-product2.svg'
+
+import Product1 from '@/components/product/Product1.vue'
+import sold from '@assets/icons/sold.svg';
 
 const items = ref([
   { title: 'Item 01',value: 1, },
@@ -23,14 +26,31 @@ const items_check = ref([
  const minPrice = 0 // Valor mínimo del rango
  const maxPrice = 1000 // Valor máximo del rango
 
+ const homeStores = useHomeStores()
 
+const data = ref(null)
+
+watchEffect(fetchData)
+
+async function fetchData() {
+
+  await homeStores.fetchData()
+  data.value = homeStores.getData
+ 
+}
+
+
+      const  valores= [20, 40]
+      
+    
+ 
 
 </script>
 
 <template>
  
 
-  <VContainer class="content py-13 text-center w-100">
+  <VContainer class="mt-10">
     
     <VRow no-gutters>
 
@@ -40,7 +60,7 @@ const items_check = ref([
               CATEGORÍAS
             </VCardItem> 
             
-            <v-list :items="items" class="text-left"></v-list>
+            <v-list :items="items" class="text-left sidebar-container"></v-list>
 
         </VCard>
 
@@ -61,44 +81,31 @@ const items_check = ref([
               ></v-text-field>
             </v-card-text>
 
-            <v-checkbox
+            <v-checkbox class="custom-check"
               v-for="(item, index) in items_check"
               :key="index"
               v-model="item.selected"
               :label="item.name"
             ></v-checkbox>
 
-            <v-row class="align-center">
-              <v-col cols="12">
-                <v-slider
-                  v-model="priceRange"
-                  :min="minPrice"
-                  :max="maxPrice"
-                  thumb-label="always"
-                  thumb-color="primary"
-                  track-color="primary"
-                ></v-slider>
-              </v-col>
-            </v-row>
-            <v-row class="align-center">
-              <v-col cols="6">
-                <v-text-field    
-                  v-model="minPrice"
-                  label="Precio Mínimo"
-                  outlined
-                  
-                ></v-text-field>
-              </v-col>
-              <v-col cols="6">
-                <v-text-field
-                  v-model="maxPrice"
-                  label="Precio Máximo"
-                  outlined
-                ></v-text-field>
-              </v-col>
-            </v-row>
-
             <VCardItem class="p-0 text-left mt-5">
+              PRICE
+            </VCardItem>
+
+            <v-row class="align-center container-vslider">
+              <v-col cols="12">
+                <v-range-slider class="custom-vslider"
+                  v-model="valores"
+                  step="10"
+                  thumb-label="always"
+                ></v-range-slider>
+              </v-col>
+            </v-row>
+            <VCardItem class="p-0 text-left mt-5">
+              Price: $10 — $200
+            </VCardItem>
+
+            <VCardItem class="p-0 text-left mt-9">
               REVIEW
             </VCardItem>
             <VCardItem class="p-0 text-left mt-5">
@@ -120,14 +127,14 @@ const items_check = ref([
         
         <VCard class="mt-7 card-information menu-prod">
 
-          <v-row align="start" no-gutters>
+          <v-row align="center" no-gutters>
 
             <v-col cols="6" class="text-left">
               <span>8 Productos</span>
             </v-col>
 
             <v-col cols="4" class="text-left">
-              <v-select
+              <v-select class="my-custom-select"
                 clearable
                 label="Select"
                 :items="['producto 1', 'producto 2', 'producto 3', 'producto 3', 'producto 4', 'producto 5']"
@@ -135,15 +142,24 @@ const items_check = ref([
               ></v-select>
             </v-col> 
 
-            <v-col cols="2" class="text-left flex pl-4 pr-2">
+            <v-col cols="2"  class="text-left flex pl-4 pr-2">
               <span>Vista</span>
-              <img :src="icon1" class="ml-3"/>
-              <img :src="icon2" class="ml-3"/>
+              <router-link to="/services">
+                <img :src="icon1" class="ml-3"/>
+              </router-link>
+              <router-link to="/services">
+                <img :src="icon2" class="ml-3"/>
+              </router-link>
 
             </v-col>  
           </v-row>
 
-        </VCard>  
+        </VCard> 
+        
+        <v-row class="align-center row-products">
+          
+        </v-row>
+        
       </VCol>
 
 
@@ -164,7 +180,66 @@ const items_check = ref([
 .menu-prod
 {
   padding: 8px 16px;
+  border-radius: 32px;
+  background-color: #E2F8FC;
 }
+.my-custom-select{
+  border-radius: 48px!important;
+  font-size: 12px!important;
+  max-height: 33px!important;
+}
+
+
+.v-select::v-deep(.v-label)
+{
+  font-size: 14px!important;
+}
+
+
+.v-select::v-deep(.v-field__field)
+{
+  height: 33px!important;
+  border-radius: 48px!important;
+ 
+}
+.v-select::v-deep(.v-input__details)
+{
+  height: 0px!important;
+}
+
+.v-select::v-deep(.v-field--appended)
+{
+  background-color:#FFFFFF;
+}
+
+
+.sidebar-container[data-v-a03b6bb9]
+{
+  background-color: #E2F8FC;
+
+}
+
+.row-products{
+  padding: 50px 30px;
+}
+
+
+.custom-check
+{
+  max-height: 50px!important;
+}
+
+.custom-vslider::deep(.v-slider-track)
+{
+  background-color: #0A1B33!important;
+}
+
+.container-vslider
+{
+  max-height: 40px!important;
+  margin-top: 24px;
+}
+
 
 
 </style>
