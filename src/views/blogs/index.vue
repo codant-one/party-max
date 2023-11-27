4a0fd2c479b379520a566c113f54cc26531d8c1c
Freddy Castro <fcastrofreelancer@gmail.com>
2023-11-27 14:21:26 -0400

    Fix Blogs

--- src/views/blogs/index.vue
+++ src/views/blogs/index.vue
@@ -2,8 +2,10 @@
 // import { ref } from 'vue'
 import blogsCard from '@/components/blogs/blogCard.vue'
 import { useMiscellaneousStores } from '@/stores/miscellaneous' 
+import bg from '@assets/images/blogs.jpg';
 
-
+const baseURL = ref(import.meta.env.VITE_APP_DOMAIN_API_URL + '/storage/')
+const backgroundStyle = ref({})
 const blogsStores = useMiscellaneousStores()
 
 const blogs = ref([])
@@ -15,24 +17,52 @@ const fetchData = async () => {
 
 fetchData()
 
+backgroundStyle.value = {
+    backgroundImage: `url(${bg})`,
+    backgroundRepeat: 'repeat'
+  }
 
 </script>
 
 <template>
  
 
-  <VContainer class="content py-13 text-center w-100">
+  <VContainer 
+    class="content py-13 text-center w-100" 
+    :style="backgroundStyle"
+    fluid=true
+  >
    
-   <blogsCard
-    v-for="blog in blogs.slice(0, 3)"
-    :id="blog.id"
-    :title="blog.title"
-    :description="blog.description"
-    :image="blog.image"
-    :user="blog.user.name"
-    :date="blog.date"
-   >
-   </blogsCard>
+    <VRow
+      class="d-flex justify-center"
+    >
+      <VCol
+        cols="7"
+      >
+         <blogsCard
+          v-for="blog in blogs.slice(0, 3)"
+          :id="blog.id"
+          :title="blog.title"
+          :description="blog.description"
+          :image="baseURL + blog.image"
+          :user="blog.user.name"
+          :date="blog.date"
+         >
+         </blogsCard>
+      </VCol>
+
+      <VCol
+        cols="3"
+      >
+        TAL
+      </VCol>
+    </VRow>
     
   </VContainer>
 </template>
+
+<style scoped>
+  .bg-image {
+      background-image: url()
+    }
+</style>
