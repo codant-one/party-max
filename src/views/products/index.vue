6938cfaf5e64fc0b67ed1ba937fd5babff827a7f
SteffaniCastro <steffani.castro.useche@gmail.com>
2024-05-17 16:14:40 -0400

    fix products

--- src/views/products/index.vue
+++ src/views/products/index.vue
@@ -838,10 +838,7 @@ const addfavorite = (product_id) => {
           </VCard>
 
           <!-- padres, hijos y nietos -->
-          <VCard 
-            class="no-shadown mt-5 card-icons tw-bg-green" 
-            v-if="route.query.fathercategory && category
-            && categories.filter(item =>item.slug === route.query.category)[0].children.filter(item =>item.slug === route.query.category + '/' + route.query.fathercategory)[0].grandchildren.length > 0">
+          <VCard  class="no-shadown mt-5 card-icons tw-bg-green" v-if="route.query.fathercategory && category">
             <VCardText 
               v-if="categories.filter(item =>item.slug === route.query.category)[0].children.filter(item =>item.slug === route.query.category + '/' + route.query.fathercategory)[0].grandchildren.length < 6"
               class="px-4 px-md-7 d-flex align-items-stretch"
@@ -858,16 +855,17 @@ const addfavorite = (product_id) => {
                       wholesalers: route.query.wholesalers === 'true' ? true : false
                     }
                   }" class="tw-no-underline d-block text-center justify-content-center zoom w-50">
-                   <img v-if="i.icon_subcategory !== null" :src="baseURL + i.icon_subcategory" class="d-block" :class="route.query.subcategory === i.slug.split('/')[2] ? 'border-theme-active' : 'border-theme'"/>
+                    <img v-if="i.icon_subcategory !== null" :src="baseURL + i.icon_subcategory" class="d-block" :class="route.query.subcategory === i.slug.split('/')[2] ? 'border-theme-active' : 'border-theme'"/>
                     <img v-else :src="t_7" class="d-block" :class="route.query.subcategory === i.slug.split('/')[2] ? 'border-theme-active' : 'border-theme'"/>
                   <span class="d-block size-theme mt-2" :class="route.query.subcategory === i.slug.split('/')[2] ? 'tw-text-primary' : 'tw-text-tertiary'">{{i.name}}</span>
                 </router-link>
               </template>
-            </VCardText> 
+            </VCardText>
             <VCardText 
               v-else
               class="px-4 d-flex align-items-stretch justify-content-center">
               <swiper
+                :initialSlide="categories.filter(item =>item.slug === route.query.category)[0].children.filter(item =>item.slug === route.query.category + '/' + route.query.fathercategory)[0].grandchildren.findIndex(item =>item.slug === route.query.category + '/' + route.query.fathercategory + '/' + route.query.subcategory)"
                 :slidesPerView="5"
                 :spaceBetween="5"
                 :navigation="true"
@@ -918,9 +916,9 @@ const addfavorite = (product_id) => {
                       wholesalers: route.query.wholesalers === 'true' ? true : false
                     }
                   }" class="tw-no-underline d-block text-center justify-content-center zoom w-50">
-                  <img v-if="i.icon_subcategory !== null" :src="baseURL + i.icon_subcategory" class="border-theme d-block"/>
-                  <img v-else :src="t_7" class="border-theme d-block"/>
-                  <span class="d-block size-theme tw-text-tertiary mt-2">{{i.name}}</span>
+                  <img v-if="i.icon_subcategory !== null" :src="baseURL + i.icon_subcategory" class="d-block" :class="route.query.subcategory === i.slug.split('/')[2] ? 'border-theme-active' : 'border-theme'"/>
+                  <img v-else :src="t_7" class="d-block" :class="route.query.subcategory === i.slug.split('/')[2] ? 'border-theme-active' : 'border-theme'"/>
+                  <span class="d-block size-theme mt-2" :class="route.query.subcategory === i.slug.split('/')[2] ? 'tw-text-primary' : 'tw-text-tertiary'">{{i.name}}</span>
                 </router-link>
               </template>
             </VCardText> 
@@ -946,9 +944,68 @@ const addfavorite = (product_id) => {
                         wholesalers: route.query.wholesalers === 'true' ? true : false
                       }
                     }" class="tw-no-underline d-block text-center justify-content-center zoom">
-                    <img v-if="i.icon_subcategory !== null" :src="baseURL + i.icon_subcategory" class="border-theme d-block"/>
-                    <img v-else :src="t_7" class="border-theme d-block"/>
-                    <span class="d-block size-theme tw-text-tertiary mt-2">{{i.name}}</span>
+                    <img v-if="i.icon_subcategory !== null" :src="baseURL + i.icon_subcategory" class="d-block" :class="route.query.subcategory === i.slug.split('/')[2] ? 'border-theme-active' : 'border-theme'"/>
+                    <img v-else :src="t_7" class="d-block" :class="route.query.subcategory === i.slug.split('/')[2] ? 'border-theme-active' : 'border-theme'"/>
+                    <span class="d-block size-theme mt-2" :class="route.query.subcategory === i.slug.split('/')[2] ? 'tw-text-primary' : 'tw-text-tertiary'">{{i.name}}</span>
+                  </router-link>
+                </swiper-slide>
+              </swiper>
+            </VCardText> 
+          </VCard>
+
+          <!-- solo padres e hijos sin nietos -->
+          <VCard 
+            class="no-shadown mt-5 card-icons tw-bg-green" 
+            v-if="typeof route.query.fathercategory === 'undefined' && 
+            typeof route.query.subcategory !== 'undefined' 
+            && route.query.category && category
+            && categories.filter(item =>item.slug === route.query.category)[0].children.filter(item =>item.slug === route.query.category + '/' + route.query.subcategory)[0].grandchildren.length === 0">
+            <VCardText 
+              v-if="categories.filter(item =>item.slug === route.query.category)[0].children.length < 6"
+              class="px-4 px-md-7 d-flex align-items-stretch"
+              :class="categories.filter(item =>item.slug === route.query.category)[0].children.length > 1 ? 'justify-content-between' : 'justify-content-center'">        
+              <template v-for="(i, index) in categories.filter(item =>item.slug === route.query.category)[0].children">
+                <router-link
+                  :to="{
+                    name: 'products',
+                    query: {
+                      category: route.query.category,
+                      subcategory: i.slug.split('/')[1],
+                      colorId: colorsSelected.join(','),
+                      wholesalers: route.query.wholesalers === 'true' ? true : false
+                    }
+                  }" class="tw-no-underline d-block text-center justify-content-center zoom w-50">
+                  <img v-if="i.icon_subcategory !== null" :src="baseURL + i.icon_subcategory" class="d-block" :class="route.query.subcategory === i.slug.split('/')[1] ? 'border-theme-active' : 'border-theme'"/>
+                  <img v-else :src="t_7" class="d-block" :class="route.query.subcategory === i.slug.split('/')[1] ? 'border-theme-active' : 'border-theme'"/>
+                  <span class="d-block size-theme mt-2" :class="route.query.subcategory === i.slug.split('/')[1] ? 'tw-text-primary' : 'tw-text-tertiary'">{{i.name}}</span>
+                </router-link>
+              </template>
+            </VCardText> 
+            <VCardText 
+              v-else
+              class="px-4 d-flex align-items-stretch justify-content-center">
+              <swiper
+                :initialSlide="categories.filter(item =>item.slug === route.query.category)[0].children.findIndex(item =>item.slug === route.query.category + '/' + route.query.subcategory)"
+                :slidesPerView="5"
+                :spaceBetween="5"
+                :navigation="true"
+                :loop="true"
+                :modules="modules"
+                class="mySwiper">
+                <swiper-slide v-for="(i, index) in categories.filter(item =>item.slug === route.query.category)[0].children" class="py-2">
+                  <router-link
+                    :to="{
+                      name: 'products',
+                      query: {
+                        category: route.query.category,
+                        subcategory: i.slug.split('/')[1],
+                        colorId: colorsSelected.join(','),
+                        wholesalers: route.query.wholesalers === 'true' ? true : false
+                      }
+                    }" class="tw-no-underline d-block text-center justify-content-center zoom">
+                    <img v-if="i.icon_subcategory !== null" :src="baseURL + i.icon_subcategory" class="d-block" :class="route.query.subcategory === i.slug.split('/')[1] ? 'border-theme-active' : 'border-theme'"/>
+                    <img v-else :src="t_7" class="d-block" :class="route.query.subcategory === i.slug.split('/')[1] ? 'border-theme-active' : 'border-theme'"/>
+                    <span class="d-block size-theme mt-2" :class="route.query.subcategory === i.slug.split('/')[1] ? 'tw-text-primary' : 'tw-text-tertiary'">{{i.name}}</span>
                   </router-link>
                 </swiper-slide>
               </swiper>
@@ -977,9 +1034,9 @@ const addfavorite = (product_id) => {
                       wholesalers: route.query.wholesalers === 'true' ? true : false
                     }
                   }" class="tw-no-underline d-block text-center justify-content-center zoom w-50">
-                  <img v-if="i.icon_subcategory !== null" :src="baseURL + i.icon_subcategory" class="border-theme d-block"/>
-                  <img v-else :src="t_7" class="border-theme d-block"/>
-                  <span class="d-block size-theme tw-text-tertiary mt-2">{{i.name}}</span>
+                  <img v-if="i.icon_subcategory !== null" :src="baseURL + i.icon_subcategory" class="d-block" :class="route.query.subcategory === i.slug.split('/')[1] ? 'border-theme-active' : 'border-theme'"/>
+                  <img v-else :src="t_7" class="d-block" :class="route.query.subcategory === i.slug.split('/')[1] ? 'border-theme-active' : 'border-theme'"/>
+                  <span class="d-block size-theme mt-2" :class="route.query.subcategory === i.slug.split('/')[1] ? 'tw-text-primary' : 'tw-text-tertiary'">{{i.name}}</span>
                 </router-link>
               </template>
             </VCardText> 
@@ -1004,9 +1061,9 @@ const addfavorite = (product_id) => {
                         wholesalers: route.query.wholesalers === 'true' ? true : false
                       }
                     }" class="tw-no-underline d-block text-center justify-content-center zoom">
-                    <img v-if="i.icon_subcategory !== null" :src="baseURL + i.icon_subcategory" class="border-theme d-block"/>
-                    <img v-else :src="t_7" class="border-theme d-block"/>
-                    <span class="d-block size-theme tw-text-tertiary mt-2">{{i.name}}</span>
+                    <img v-if="i.icon_subcategory !== null" :src="baseURL + i.icon_subcategory" class="d-block" :class="route.query.subcategory === i.slug.split('/')[1] ? 'border-theme-active' : 'border-theme'"/>
+                    <img v-else :src="t_7" class="d-block" :class="route.query.subcategory === i.slug.split('/')[1] ? 'border-theme-active' : 'border-theme'"/>
+                    <span class="d-block size-theme mt-2" :class="route.query.subcategory === i.slug.split('/')[1] ? 'tw-text-primary' : 'tw-text-tertiary'">{{i.name}}</span>
                   </router-link>
                 </swiper-slide>
               </swiper>
