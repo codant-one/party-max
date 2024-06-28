abfa7e6c7e99461190f4df9b69ae3b49741ff10b
SteffaniCastro <steffani.castro.useche@gmail.com>
2024-06-28 18:25:49 -0400

    fix

--- src/components/cart/Stepper.vue
+++ src/components/cart/Stepper.vue
@@ -348,7 +348,7 @@ watchEffect(() => {
 
 @media only screen and (max-width: 767px) {
   .stepper-title {
-    font-size: 14px !important;
+    font-size: 13px !important;
   }
 
   .stepper-steps-completed path {
