13a99efa48c226e2553cef757320d0593bdd1569
SteffaniCastro <steffani.castro.useche@gmail.com>
2024-07-19 01:50:51 -0400

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,7 +29,8 @@ export default defineNuxtConfig({
   ],
 
   device: {
-    refreshOnResize: true
+    refreshOnResize: true,
+    defaultUserAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
   },
 
   build: {
