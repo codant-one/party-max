ba1cb9f17e01ed4f88f823c89c757cbf8cdfec6d
SteffaniCastro <steffani.castro.useche@gmail.com>
2024-07-19 18:18:51 -0400

    fix

--- utils/loadCategories.ts
+++ utils/loadCategories.ts
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 export async function loadCategories() {
 
-  console.log(process.env.NUXT_APP_DOMAIN_API_URL+ '/api/' +' home')
-  const response = await axios.get(process.env.NUXT_APP_DOMAIN_API_URL+ '/api/' +' home');
+  console.log(process.env.NUXT_APP_DOMAIN_API_URL+ '/api/' +'home')
+  const response = await axios.get(process.env.NUXT_APP_DOMAIN_API_URL+ '/api/' +'home');
 
   return response?.data?.data;
 
