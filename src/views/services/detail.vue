f32a77c6353e203397441fb15f0721cb53f43c1e
SteffaniCastro <steffani.castro.useche@gmail.com>
2025-06-10 00:52:14 -0400

    fix hover

--- src/views/services/detail.vue
+++ src/views/services/detail.vue
@@ -1263,6 +1263,10 @@ const buildEmbedUrl = (url) => {
     pointer-events: auto;
   }
 
+  .mySwiper2 iframe {
+    pointer-events: none !important;
+  }
+
   .zoom-in:hover {
     cursor: zoom-in;
   }
