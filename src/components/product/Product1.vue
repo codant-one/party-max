000f2f83adaaeb7b23bd823c981cc9f816adc4b7
SteffaniCastro <steffani.castro.useche@gmail.com>
2025-06-29 20:53:41 -0400

    fix

--- src/components/product/Product1.vue
+++ src/components/product/Product1.vue
@@ -40,7 +40,7 @@ watchEffect(() => {
         wholesale_price.value = props.product.wholesale_price ?? '0.00'
         price_for_sale.value = props.product.price_for_sale
         name.value = props.product.name.toLowerCase().replace(/(^|\s)\p{L}/gu, (match) => match.toUpperCase());
-        store.value = props.product.store ?? (props.product.company ?? props.product.user)
+        store.value = props.product.user.user_detail.store_name ?? (props.product.supplier?.company_name ?? (props.product.user.name + ' ' + (props.product.user.last_name ?? '')))
         rating.value = props.product.rating
         slug.value = props.product.slug
         stock.value = props.product.first_color.stock
--- src/components/service/Service1.vue
+++ src/components/service/Service1.vue
@@ -29,9 +29,9 @@ watchEffect(() => {
 
     if (!(Object.entries(props.service).length === 0) && props.service.constructor === Object) {
         image.value = props.service.image
-        price.value = props.service.cupcakes.length > 0 ? props.service.cupcakes[0].price : props.service.price
+        price.value = props.service.first_cupcake ? props.service.first_cupcake.price : props.service.price
         name.value = props.service.name.toLowerCase().replace(/(^|\s)\p{L}/gu, (match) => match.toUpperCase());
-        store.value = props.service.user.user_detail.store_name ?? (props.service.user.supplier?.company_name ?? (props.service.user.name + ' ' + (props.service.user.last_name ?? '')))
+        store.value = props.service.store ?? (props.service.company ?? props.service.user)
         rating.value = props.service.rating
         slug.value = props.service.slug
     }
