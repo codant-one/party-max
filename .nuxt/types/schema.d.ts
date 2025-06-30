0ec226cc8d718a3ade717cf3ae0c0ff1e4a8822f
Klengery Rodriguez <klengery@gmail.com>
2025-06-30 16:08:14 -0400

    .

---
+++ .nuxt/types/schema.d.ts
@@ -0,0 +1,54 @@
+import { NuxtModule, RuntimeConfig } from '@nuxt/schema'
+declare module '@nuxt/schema' {
+  interface NuxtConfig {
+    ["pinia"]?: typeof import("@pinia/nuxt").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>,    ["nuxt-scrollbar"]?: typeof import("@davestewart/nuxt-scrollbar").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>,    ["device"]?: typeof import("@nuxtjs/device").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>,    ["devtools"]?: typeof import("@nuxt/devtools").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>,    ["telemetry"]?: typeof import("@nuxt/telemetry").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>,  modules?: (undefined | null | false | NuxtModule | string | [NuxtModule | string, Record<string, any>] | ["@pinia/nuxt", Exclude<NuxtConfig["pinia"], boolean>] | ["@davestewart/nuxt-scrollbar", Exclude<NuxtConfig["nuxt-scrollbar"], boolean>] | ["@nuxtjs/device", Exclude<NuxtConfig["device"], boolean>] | ["@nuxt/devtools", Exclude<NuxtConfig["devtools"], boolean>] | ["@nuxt/telemetry", Exclude<NuxtConfig["telemetry"], boolean>])[],
+  }
+}
+declare module 'nuxt/schema' {
+  interface NuxtConfig {
+    ["pinia"]?: typeof import("@pinia/nuxt").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>,    ["nuxt-scrollbar"]?: typeof import("@davestewart/nuxt-scrollbar").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>,    ["device"]?: typeof import("@nuxtjs/device").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>,    ["devtools"]?: typeof import("@nuxt/devtools").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>,    ["telemetry"]?: typeof import("@nuxt/telemetry").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>,  modules?: (undefined | null | false | NuxtModule | string | [NuxtModule | string, Record<string, any>] | ["@pinia/nuxt", Exclude<NuxtConfig["pinia"], boolean>] | ["@davestewart/nuxt-scrollbar", Exclude<NuxtConfig["nuxt-scrollbar"], boolean>] | ["@nuxtjs/device", Exclude<NuxtConfig["device"], boolean>] | ["@nuxt/devtools", Exclude<NuxtConfig["devtools"], boolean>] | ["@nuxt/telemetry", Exclude<NuxtConfig["telemetry"], boolean>])[],
+  }
+  interface RuntimeConfig {
+   app: {
+      buildId: string,
+
+      baseURL: string,
+
+      buildAssetsDir: string,
+
+      cdnURL: string,
+   },
+
+   nitro: {
+      envPrefix: string,
+   },
+  }
+  interface PublicRuntimeConfig {
+   APP_VERBOSE: string,
+
+   APP_DOMAIN_API_URL: string,
+
+   APP_DOMAIN_ADMIN_URL: string,
+
+   MY_DOMAIN: string,
+
+   GOOGLE_MANAGER: string,
+
+   GOOGLE_TAG_MANAGER: string,
+
+   NODE_ENV: string,
+
+   device: {
+      enabled: boolean,
+
+      defaultUserAgent: string,
+
+      refreshOnResize: boolean,
+   },
+  }
+}
+declare module 'vue' {
+        interface ComponentCustomProperties {
+          $config: RuntimeConfig
+        }
+      }
