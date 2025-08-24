fd4b82949ce986ecf8ea373076c7199c32651ed7
SteffaniCastro <steffani.castro.useche@gmail.com>
2025-08-24 17:07:51 -0400

    fix view content

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,6 +14,9 @@ export default defineNuxtConfig({
       },
       link: [
         { rel: 'icon', type: 'image/png', href: '/favicon.png' }
+      ],
+      meta: [
+        { property: 'fb:app_id', content: '399759509371752' }
       ]
     }
   },
